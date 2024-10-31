c6f96095a2e18949e7c30dd5a2d4205540db56ea
Adrian Kühni <adrian.kuehni@gmail.com>
2024-11-01 00:34:11 +0100

    Initial project setup with JakartaFaces

---
+++ src/main/java/dev/kuehni/jeecms/util/config/FacesActivator.java
@@ -0,0 +1,8 @@
+package dev.kuehni.jeecms.util.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class FacesActivator {}
