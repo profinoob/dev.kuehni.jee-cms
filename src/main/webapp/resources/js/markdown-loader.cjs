eac4b3008ad18446907ce064d26940b027b1ea63
Adrian Kühni <adrian.kuehni@gmail.com>
2024-11-08 01:00:20 +0100

    Consistent style (font, colors)

--- src/main/webapp/resources/js/markdown-loader.cjs
+++ src/main/webapp/resources/js/markdown-loader.cjs
@@ -1,9 +1,21 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const containers = document.querySelectorAll('.cms-markdown');
     if (containers && containers.length) {
-        const {marked} = await import('https://cdnjs.cloudflare.com/ajax/libs/marked/14.1.3/lib/marked.esm.js');
+        const {
+            marked,
+            Renderer
+        } = await import('https://cdnjs.cloudflare.com/ajax/libs/marked/14.1.3/lib/marked.esm.js');
+
+        // Custom renderers for primefaces style
+        const renderer = new Renderer();
+        renderer.link = ({href, text, title}) => {
+            const titleAttr = title ? ` title="${title}"` : '';
+            return `<a href="${href}" class="ui-link ui-widget"${titleAttr}>${text}</a>`;
+        };
+
         marked.use({
             breaks: true,
+            renderer,
         });
 
         containers.forEach(container => {
